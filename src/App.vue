2a277abe0aa6945c891380b00f572667dc52531f
Joso026 <83786526+Joso026@users.noreply.github.com>
2025-03-31 22:22:32 -0300

    Inicialização do projeto VueJs com os recursos basicos

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <h1>Pomodoro Timer</h1>
+    <AppTimer />
+  </div>
+</template>
+
+<script>
+import AppTimer from './components/AppTimer.vue';
+
+export default {
+  components: {
+    AppTimer,
+  }
+}
+</script>
+
+<style>
+#app {
+  text-align: center;
+  font-family: Arial, Helvetica, sans-serif;
+  padding: 20px;
+}
+</style>
